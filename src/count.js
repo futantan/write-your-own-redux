ca9b4ab9c3b26e2fb1dbb5787a6366520402582e
Tantan Fu <luckytantanfu@gmail.com>
2019-06-12 23:01:35 +0800

    implement createStore

--- src/count.js
+++ src/count.js
@@ -1,12 +1,23 @@
-const createStore = () => {
-  const getState = () => {}
-  const subscribe = () => {}
-  const dispatch = () => {}
+const createStore = (reducer, state) => {
+  let storeState = state;
+  let listeners = [];
+
+  const getState = () => storeState;
+  const dispatch = action => {
+    storeState = reducer(storeState, action);
+    listeners.forEach(listener => listener());
+  };
+  const subscribe = listener => {
+    listeners = [...listeners, listener];
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
 
   return {
     getState,
-    subscribe,
     dispatch,
+    subscribe
   };
 };
 
@@ -21,7 +32,7 @@ function counter(state = 0, action) {
   }
 }
 
-const store = createStore(counter);
+const store = createStore(counter, 0);
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
@@ -30,3 +41,4 @@ store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "DECREMENT" });
 unsubscribe();
 store.dispatch({ type: "DECREMENT" });
+console.log(store.getState());
